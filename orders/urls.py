0d46a18e93c9f24490ecff2d085e776763a093bf
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-13 15:15:00 -0600

    intial commit

---
+++ orders/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+from . import views
+
+
+urlpatterns = [
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+ 
+
+
