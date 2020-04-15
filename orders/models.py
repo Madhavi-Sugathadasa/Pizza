24a620cee9c2d6c65bb30ce950e97c4c9a9ebbba
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-15 13:30:00 -0600

    add Menu item model

--- orders/models.py
+++ orders/models.py
@@ -8,3 +8,16 @@ class Menu_Type(models.Model):
     
     def __str__(self):
         return f"{self.name} - order:{self.display_order}"
+
+    
+class Menu_Item(models.Model):
+    type_id = models.ForeignKey(Menu_Type,on_delete=models.CASCADE,related_name="menu_types")
+    item_name = models.CharField(max_length=64)
+    image = models.ImageField(upload_to ='Media')
+    no_of_toppings = models.IntegerField(default=0)
+    is_mult_cat = models.BooleanField(default=False)
+    small_price = models.FloatField(null=True, blank=True)
+    large_price = models.FloatField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.type_id.name} - {self.item_name}"
