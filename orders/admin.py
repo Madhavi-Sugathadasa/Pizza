c76d24c077ff6e2266e0289aec8130ada50c794c
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-17 15:35:00 -0600

    register Menu item model

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu_Type
+from .models import Menu_Type, Menu_Item
 
 admin.site.register(Menu_Type)
+admin.site.register(Menu_Item)
