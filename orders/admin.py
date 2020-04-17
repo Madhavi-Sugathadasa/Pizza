7a059bb7480fb5ca654c2b7a7779ffaa6a629c9c
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-17 15:35:00 -0600

    register Topping model

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu_Type, Menu_Item
+from .models import Menu_Type, Menu_Item, Topping
 
 admin.site.register(Menu_Type)
 admin.site.register(Menu_Item)
+admin.site.register(Topping)
