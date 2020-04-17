b5b9cedcbcb8950613c59c7fef9767174758e683
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-17 15:35:00 -0600

    register Addition model

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu_Type, Menu_Item, Topping
+from .models import Menu_Type, Menu_Item, Topping, Addition
 
 admin.site.register(Menu_Type)
 admin.site.register(Menu_Item)
 admin.site.register(Topping)
+admin.site.register(Addition)
