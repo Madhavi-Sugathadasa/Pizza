e7e264543dd39da581a324a04fccb404328d04f1
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-17 15:35:00 -0600

    register Order item model

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu_Type, Menu_Item, Topping, Addition, Cart, Order
+from .models import Menu_Type, Menu_Item, Topping, Addition, Cart, Order, Order_Item
 
+# Register your models here.
 admin.site.register(Menu_Type)
 admin.site.register(Menu_Item)
 admin.site.register(Topping)
 admin.site.register(Addition)
 admin.site.register(Cart)
 admin.site.register(Order)
+admin.site.register(Order_Item)
