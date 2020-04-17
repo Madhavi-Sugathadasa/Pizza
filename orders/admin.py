85f363b879ccefa8dac12828d36a9b89702e0e43
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-04-17 15:35:00 -0600

    register Cart model

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Menu_Type, Menu_Item, Topping, Addition
+from .models import Menu_Type, Menu_Item, Topping, Addition, Cart
 
 admin.site.register(Menu_Type)
 admin.site.register(Menu_Item)
 admin.site.register(Topping)
 admin.site.register(Addition)
+admin.site.register(Cart)
