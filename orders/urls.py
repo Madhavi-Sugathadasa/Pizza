8a27b10b30b0dfdd1cbc0056ab0f906b568f0e9a
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-04 16:47:00 -0600

    update urls

--- orders/urls.py
+++ orders/urls.py
@@ -10,6 +10,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
+    path("register", views.register_view, name="register"),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
